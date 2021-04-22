229bffd50690d4a620b44a7e4ee3c0b8165a4648
Ringo Hoffmann <riho@live.de>
2021-04-22 23:51:22 +0200

    add short example; update readme

--- examples/short/main.go
+++ examples/short/main.go
@@ -1,18 +1,3 @@
-# shireikan &nbsp; [![](https://img.shields.io/badge/docs-pkg.do.dev-blue?logo=go&logoColor=white)](https://pkg.go.dev/github.com/zekroTJA/shireikan?tab=doc) ![CI](https://github.com/zekroTJA/shireikan/workflows/CI/badge.svg) [![Coverage Status](https://coveralls.io/repos/github/zekroTJA/shireikan/badge.svg?branch=master)](https://coveralls.io/github/zekroTJA/shireikan?branch=master) [![Go Report Card](https://goreportcard.com/badge/github.com/zekroTJA/shireikan)](https://goreportcard.com/report/github.com/zekroTJA/shireikan)
-
-司令官 - A prototype, object-oriented command handler for discordgo.
-
-This is a work-in-progress command handler which shall later replace the current command handler used in [shinpuru](https://github.com/zekroTJA/shinpuru).
-
-If you are interested how to use this package, see the [basic example](examples/basic).
-
-This command handler is strongly inspired by [Lukaesebrot's](https://github.com/Lukaesebrot) package [dgc](https://github.com/Lukaesebrot/dgc).
-
----
-
-## Example
-
-```go
 package main
 
 import (
@@ -116,14 +101,3 @@ func main() {
 
 	handler.Setup(session)
 }
-```
-
-You can find more complex examples in the [`examples/`](examples) directory.
-
-shireikan is used in my own Discord bot [**shinpuru**](https://github.com/zekroTJA/shinpuru).
-Take a look if you want to see a "real world" implementation example. 😉
-
----
-
-© 2020-2021 Ringo Hoffmann (zekro Development).  
-Covered by the MIT Licence.
